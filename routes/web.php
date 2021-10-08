f46aaa282ec5bff57efc212a5cc6fa077cbb078e
Eddy Ramos <eddyinf605@gmail.com>
2021-10-08 16:58:37 -0400

    se agrego un mensaje

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+/* hola como estas aqui unsaludo */
 
 Route::resource('articulos', 'App\Http\Controllers\ArticuloController');
 
