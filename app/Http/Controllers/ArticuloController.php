7a9f0728cf8f54b0f2fea94ec7de8b465c1cfc81
Eddy Ramos <eddyinf605@gmail.com>
2021-10-08 17:24:10 -0400

    controlador de articulos

--- app/Http/Controllers/ArticuloController.php
+++ app/Http/Controllers/ArticuloController.php
@@ -34,6 +34,7 @@ class ArticuloController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
+    /**controlador de articulos */
     public function store(Request $request)
     {
         $articulos = new Articulo();
