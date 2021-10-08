bbd6735bbe7a957bc5639a08ba2200612e86bd00
Eddy Ramos <eddyinf605@gmail.com>
2021-10-08 16:52:35 -0400

    subiendo el primer proyecto en laravel

---
+++ resources/views/articulo/index.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.plantillabase');
+
+@section('contenido')
+<a href="articulos/create" class="btn btn-primary">CREAR</a>
+
+<table class="table table-dark table-striped mt-4">
+    <thead>
+        <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Código</th>
+            <th scope="col">Descripción</th>
+            <th scope="col">Cantidad</th>
+            <th scope="col">Precio</th>
+            <th scope="col">Acciones</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($articulos as $articulo)
+        <tr>
+            <td>{{$articulo->id}}</td>
+            <td>{{$articulo->codigo}}</td>
+            <td>{{$articulo->descripcion}}</td>
+            <td>{{$articulo->cantidad}}</td>
+            <td>{{$articulo->precio}}</td>
+            <td>
+                <a class="btn btn-info"> Editar</a>
+                <button class="btn btn-danger">Borrar</button>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+@endsection
