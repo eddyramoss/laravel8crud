38786376f1970b12b091d3674af1216c03e9c21a
Eddy Ramos <eddyinf605@gmail.com>
2021-10-08 17:10:45 -0400

    se cambio controladores

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+/*controladores de los articulos */
+
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
